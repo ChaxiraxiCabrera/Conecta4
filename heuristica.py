4229f8256171c76571aa36c879078eb3b576e3ed
Chaxi <chaxigonzalez@hotmail.com>
2016-05-27 18:38:34 +0100

    Modificación clase k_in_row (division en modulos)

--- heuristica.py
+++ heuristica.py
@@ -6,8 +6,8 @@ from utils import *
 
 def memoize(f):
     memo = {}
+
     def helper(state):
-        #key = (state.utility, state.to_move, frozenset(state.board.items()))
         key = tuple(state.board.items())
         if key not in memo:
             memo[key] = f(state)
@@ -21,90 +21,77 @@ def legal_moves(state):
     return [(x, y) for (x, y) in state.moves
             if y == 1 or (x, y - 1) in state.board]
 
-def k_in_row(state, player, (delta_x, delta_y)):
-    board = state.board
+
+def calculate_weights(state, pos):
     # Con los pesos favorecemos jugar abajo y en el centro del tablero
     weights_c = [4, 8, 16, 32, 16, 8, 4]
     weights_f = [32, 16, 8, 4, 2, 1]
+    # Es un hueco
+    if pos in state.moves:
+        result = weights_c[pos[0] - 1] * weights_f[pos[1] - 1] / 2
+    # Es una ficha
+    else:
+        result = weights_c[pos[0] - 1] * weights_f[pos[1] - 1]
+    return result
+
+
+def calculate_delta(move, op, (delta_x, delta_y)):
+    if op == '+':
+        pos = (move[0] + delta_x, move[1] + delta_y)
+    else:
+        pos = (move[0] - delta_x, move[1] - delta_y)
+    return pos
+
+
+def k_in_row(state, player, op, (delta_x, delta_y)):
+    board = state.board
     htotal = 0
-    h1 = 0
-    h2 = 0
     for move in legal_moves(state):
-        pos = (move[0] + delta_x, move[1] + delta_y)
+        pos = calculate_delta(move, op, (delta_x, delta_y))
+        h = 0
         good = 0
         # Mientras sea una posicion legal calculamos su valor para un maximo de 3 desplazamientos
         while (pos in state.moves or board.get(pos) == player) and good < 3:
             # Si la posicion nos pertenece
             if board.get(pos) == player:
-                # Si la ficha anterior es nuestra
-                if board.get(pos[0] - delta_x, pos[1] - delta_y) == player:
-                    h1 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1] * 2
-                # Si la ficha anterior es un hueco
-                else:
-                    h1 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1]
+                h += calculate_weights(state, pos)
             # Si la posicion es un hueco
             elif pos in state.moves:
-                # Si la ficha anterior es nuestra
-                if board.get(pos[0] - delta_x, pos[1] - delta_y) == player:
-                    h1 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1]
-                # Si la ficha anterior es un hueco
-                else:
-                    h1 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1] / 2
+                h += calculate_weights(state, pos)
             # Actualizamos las posiciones
-            pos = (pos[0] + delta_x, pos[1] + delta_y)
+            pos = calculate_delta(move, op, (delta_x, delta_y))
             good += 1
         # Si es un potencial 4 en raya sumamos su valor heuristico
         if good == 3:
-            htotal += h1
-        # Reiniciamos a la posicion original
-        pos = (move[0] + delta_x, move[1] + delta_y)
-        good = 0
-        h1 = 0
-        # Mientras sea una posicion legal calculamos su valor para un maximo de 3 desplazamientos
-        while (pos in state.moves or board.get(pos) == player) and good < 3:
-            # Si la posicion nos pertenece
-            if board.get(pos) == player:
-                # Si la ficha anterior es nuestra
-                if board.get(pos[0] + delta_x, pos[1] + delta_y) == player:
-                    h2 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1] * 2
-                # Si la ficha anterior es un hueco
-                else:
-                    h2 += weights_c[pos[0] - 1] * weights_f[pos[1] - 1]
-            # Si la posicion es un hueco
-            elif pos in state.moves:
-                # Si la ficha anterior es nuestra
-                if board.get(pos[0] + delta_x, pos[1] + delta_y) == player:
-                    h2 += (weights_c[pos[0] - 1] * weights_f[pos[1] - 1])
-                # Si la ficha anterior es un hueco
-                else:
-                    h2 += (weights_c[pos[0] - 1] * weights_f[pos[1] - 1]) / 2
-            # Actualizamos las posiciones
-            pos = (pos[0] - delta_x, pos[1] - delta_y)
-            good += 1
-        # Si es un potencial 4 en raya sumamos su valor heuristico
-        if good == 3:
-            htotal += h2
-        h2 = 0
+            htotal += h
     return htotal
 
 
-#Calcula segun el jugador usando move
+# Calcula segun el jugador usando k_in_row
 def compute_utility(state, player):
-        Pk = 0
-        Pk += k_in_row(state, player, (0, 1))
-        Pk += k_in_row(state, player, (1, 0))
-        Pk += k_in_row(state, player, (1, -1))
-        Pk += k_in_row(state, player, (-1, 1))
+        pk = 0
+        pk += k_in_row(state, player, '+', (0, 1))
+        pk += k_in_row(state, player, '+', (1, 0))
+        pk += k_in_row(state, player, '+', (1, -1))
+        pk += k_in_row(state, player, '+', (-1, 1))
+        pk += k_in_row(state, player, '-', (0, 1))
+        pk += k_in_row(state, player, '-', (1, 0))
+        pk += k_in_row(state, player, '-', (1, -1))
+        pk += k_in_row(state, player, '-', (-1, 1))
         if player == 'X':
             contrincante = 'O'
         else:
             contrincante = 'X'
-        Pk -= k_in_row(state, contrincante, (0, 1))
-        Pk -= k_in_row(state, contrincante, (1, 0))
-        Pk -= k_in_row(state, contrincante, (1, -1))
-        Pk -= k_in_row(state, contrincante, (-1, 1))
+        pk -= k_in_row(state, contrincante, '+', (0, 1))
+        pk -= k_in_row(state, contrincante, '+', (1, 0))
+        pk -= k_in_row(state, contrincante, '+', (1, -1))
+        pk -= k_in_row(state, contrincante, '+', (-1, 1))
+        pk -= k_in_row(state, contrincante, '-', (0, 1))
+        pk -= k_in_row(state, contrincante, '-', (1, 0))
+        pk -= k_in_row(state, contrincante, '-', (1, -1))
+        pk -= k_in_row(state, contrincante, '-', (-1, 1))
+        return pk
 
-        return Pk
 
 @memoize
 # FACIL: Retorna siempre 0
@@ -130,6 +117,6 @@ def h_2(state):
     if state.utility != 0:
         return state.utility * infinity
     else:
-        #calcula el valor heuristico para el jugador
+        # Calcula el valor heuristico para el jugador
         h = compute_utility(state, 'X')
         return h
